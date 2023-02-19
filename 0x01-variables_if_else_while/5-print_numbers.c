c7d543249a35eb7a20031283168d3216b718af86
WilliamNgumbao <williambaloni@gmail.com>
2023-02-19 13:54:56 -0800

    Printing numbers from 0 to 9

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - name of my function and entry point
+ *
+ * Description: Prints all single digit numbers of base 10, starting from 0 up to 9
+ *
+ * Return: 0 means it's successful
+ */
+
+int main(void)
+{
+	puts("0123456789");
+	return (0);
+}
