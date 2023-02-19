d2fbebe4465065037b216ba45335c92e2a42d8f3
WilliamNgumbao <williambaloni@gmail.com>
2023-02-19 10:35:20 -0800

    Printing size of various types

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -10,10 +10,10 @@
 
 int main(void)
 {
-	puts("Size of a char: %lu", sizeof(char));
-	puts("Size of an int: %lu", sizeof(int));
-	puts("Size of a long int: %lu", sizeof(long int));
-	puts("Size of a long long int: %lu", sizeof(long long int));
-	puts("Size of a float: %lu", sizeof(float));
+	printf("Size of a char: %lu byte(s)\n", sizeof(char));
+	printf("Size of an int: %lu byte(s)\n", sizeof(int));
+	printf("Size of a long int: %lu byte(s)\n", sizeof(long int));
+	printf("Size of a long long int: %lu byte(s)\n", sizeof(long long int));
+	printf("Size of a float: %lu byte(s)\n", sizeof(float));
 	return (0);
 }
