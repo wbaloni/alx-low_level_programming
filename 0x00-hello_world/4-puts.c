47def81e6d9a0f58ec94938bb6ca5c696fb2c955
WilliamNgumbao <williambaloni@gmail.com>
2023-02-19 09:54:18 -0800

    My first C code using the puts function

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,2 +1,19 @@
-#!/bin/bash
-puts(""Programming is like building a multilingual puzzle"); return 0; 
+#include <stdio.h>
+
+/**
+ * main - Entry Point
+ *
+ * Description: This code tests the use of the puts function
+ *
+ * Return: Always 0(Success)
+ */
+
+int main(void)
+{
+	puts(" \"Programming is like building a multilingual puzzle ");
+	return (0);
+	;
+}
+
+
+
