bc4fd35073b4cc9812594654335ddeeb1184531e
WilliamNgumbao <williambaloni@gmail.com>
2023-02-19 10:04:06 -0800

    This is my first c code using the printf function

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: This program tests the use of the printf function
+ *
+ * Return: Always 0 (Successful)
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
+
