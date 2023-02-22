b66ca96c4cbd3108dae162002851a8dc8059b4c8
WilliamNgumbao <williambaloni@gmail.com>
2023-02-21 16:42:42 -0800

    Using _putchar to print _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,24 @@
+#include "mystuff.h"
+
+/**
+ * main - the name of the function
+ *
+ * Description: this code prints a line without using printf or puts
+ *
+ * Return: 0 when successful
+ */
+
+int main(void)
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+	return (0);
+}
+
