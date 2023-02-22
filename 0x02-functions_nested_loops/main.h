a49ca6a702bf75501184f144ccd7a92d4e28c769
WilliamNgumbao <williambaloni@gmail.com>
2023-02-21 22:33:37 -0800

    my header file

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -3,3 +3,17 @@
  */
 int _putchar(char c);
 
+/**
+ * print_alphabet - prints the alphabet
+ */
+void print_alphabet(void);
+
+/**
+ * print_alphabet_x10 - prints alphabet 10 times
+ */
+void print_alphabet_x10(void);
+
+/**
+ * int_islower - checks for lowercase
+ */
+int _islower(int c);
