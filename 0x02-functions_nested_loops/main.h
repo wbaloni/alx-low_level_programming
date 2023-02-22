5ae10ac1ab281a1fd055e481ee20fa72dc1525af
WilliamNgumbao <williambaloni@gmail.com>
2023-02-22 03:28:03 -0800

    my header file

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -14,6 +14,48 @@ void print_alphabet(void);
 void print_alphabet_x10(void);
 
 /**
- * int_islower - checks for lowercase
+ * _islower - checks for lowercase
  */
 int _islower(int c);
+
+/**
+ * _isalpha - checks for alphabetic character
+ */
+int _isalpha(int c);
+
+/**
+ * print_sign - prints sign of a number
+ */
+int print_sign(int n);
+
+/**
+ *_abs - prints absolute value
+ */
+int _abs(int);
+
+/**
+ * print_last_digit - prints the last digit of a number
+ */
+int print_last_digit(int);
+
+/**
+ * jack_bauer - prints every minute of the day
+ */
+void jack_bauer(void);
+
+/**
+ * times_table - prints the 9 times table
+ */
+void times_table(void);
+
+/**
+ * add - adds two integers
+ */
+int add(int, int);
+
+/**
+ * print_to_98 - prints all natural numbers upto 98
+ */
+void print_to_98(int n);
+
+
